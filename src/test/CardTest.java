39f730b39d38523c3bfd11f50a3edee5ccff294c
AndraCarla <113671176+AndraCarla@users.noreply.github.com>
2022-12-01 10:45:16 +0100

    tests for cards

--- src/test/CardTest.java
+++ src/test/CardTest.java
@@ -1,7 +1,24 @@
-package test;
+package main;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 
 public class CardTest {
-    
-    
+    @Test
+    public void testCouleur1(){
+        Card card = new Card("Y",9);
+        assertEquals("Y",card.getCouleur());
+    }
+    @Test
+    public void testCouleur2(){
+        Card card = new Card(null,9);
+        assertEquals(null,card.getCouleur());
+    }
+    @Test
+    public void testValeur(){
+        Card card = new Card("Y",9);
+        assertEquals(9,card.getValeur());
+    }
 }
